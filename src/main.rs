11b1e43cec3bd6d45f089adddf13b25878151d09
Andelf <andelf@gmail.com>
2023-02-24 03:15:45 +0800

    add simple demo

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,16 @@ fn main() -> Result<()> {
     let _ = link.send_command(commands::GetChipId);
     //    println!("=> {:02x?}", r);
 
+    /// reset csr
+    link.send_command(DmiOp::write(0x10, 0x80000001))?;
+    link.send_command(DmiOp::write(0x10, 0x80000001))?;
+    link.send_command(DmiOp::write(0x10, 0x00000001))?;
+    link.send_command(DmiOp::write(0x04, 0x00000000))?;
+    link.send_command(DmiOp::write(0x17, 0x00220f12))?; // 0xf12 marchid
+    link.send_command(DmiOp::read(0x16))?;
+    let marchid = link.send_command(DmiOp::read(0x04))?;
+    println!("marchid => {:08x}", marchid.data);
+
     /// resume sequence
     link.send_command(DmiOp::write(0x10, 0x80000001))?;
     link.send_command(DmiOp::write(0x10, 0x80000001))?;
